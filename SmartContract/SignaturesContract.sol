59d80d63b198479089e6224d0553f8061c281717
Michael Hofmeier <michael.hofmeier@unibw.de>
2023-02-23 11:26:20 +0100

    ...

---
+++ SmartContract/SignaturesContract.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.25 <0.9.0;
+
+contract SignaturesContract {
+    
+    uint64 public lastSignatureId = 0;
+    uint64 public lastDocumentId = 0;
+
+    struct Document {
+        uint64 id;
+        string hashCode;
+        uint timestamp;
+    }
+    struct Signature {
+        uint64 id;
+        string hashCode;
+        uint timestamp;
+    }
+
+    mapping(uint64 => Document) public documents;
+    mapping(uint64 => Signature) public signatures;
+
+    constructor() {
+        
+    }
+
+    event DocumentCreated(uint64 _id);
+    event DocumentUpdated(uint64 _id, uint _timestamp);
+    event SignatureCreated(uint64 _id, uint _timestamp);
+
+    function createDocument() public returns (uint64 id) {
+        lastDocumentId++;
+        documents[lastDocumentId] = Document(lastDocumentId, "", 0);
+        emit DocumentCreated(lastDocumentId);
+        return lastDocumentId;
+    }
+    function updateDocument(uint64 _id, string memory _hashCode) public returns (uint timestamp) {
+        uint time = block.timestamp;
+        if(documents[_id].timestamp == 0){
+            documents[_id].hashCode = _hashCode;
+            documents[_id].timestamp = time;
+            emit DocumentUpdated(_id, time);
+            return time;
+        }
+        return 0;
+    }
+    function requestDocument(uint64 _id) public view returns (string memory hashCode, uint timestamp) {
+        return (documents[_id].hashCode, documents[_id].timestamp);
+    }
+
+    function createSignature(string memory _hashCode) public returns (uint64 id, uint timestamp) {
+        uint time = block.timestamp;
+        lastSignatureId++;
+        signatures[lastSignatureId] = Signature(lastSignatureId, _hashCode, time);
+        emit SignatureCreated(lastSignatureId, time);
+        return (lastSignatureId, time);
+    }
+    function requestSignature(uint64 _id) public view returns (string memory hashCode, uint timestamp) {
+        return (signatures[_id].hashCode, signatures[_id].timestamp);
+    }
+
+}
